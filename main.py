fe7ee3b23b7a8a42a6ae3a34b0174b3ddc34b58a
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-09-07 17:11:18 -0300

    gerando grafico comparativo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 from lxml import html
 import pandas as pd
-
+import plotly.graph_objects as go
 
 endereco_time= {'al-hilal':'al-hilal/21895','al-taawoun':'al-taawoun/56021','al-ittihad':'al-ittihad/34315','al-ahli':'/al-ahli/34469','al-ettifaq':'al-ettifaq/34318','al-nassr':'al-nassr/23400','al-fateh':'al-fateh/56023','al-fayha':'al-fayha/168094','al-wehda':'al-wehda/32994','abha':'abha/168090','al-tai':'al-tai/168072','al-khaleej':'al-khaleej/167228','al-akhdood':'al-akhdood/336456','al-raed':'al-raed/56031','al-riyadh':'al-riyadh/168088','damac-fc':'damac-fc/204126','al-shabab':'al-shabab/34313','al-hazem':'al-hazem/168086'}
 
@@ -31,14 +31,14 @@ def escolhe_time(time:str):
     url_23=base_api + id_time + middle_api + end_point_23 + end_api
     url_21=base_api + id_time + middle_api + end_point_21 + end_api
     
-    lista_urls=[url_23]
+    lista_urls=[url_21,url_22]
     
     for url in lista_urls:
         api_link= requests.get(url,headers=browser).json()
         if not 'error' in api_link:
             lista_dados.append(api_link['statistics'])
             if lista_urls.index(lista_urls[cont_url_list])==0:
-                lista_dados[cont_data_list]['ano'] =2023
+                lista_dados[cont_data_list]['ano'] =2021
             elif lista_urls.index(lista_urls[cont_url_list])==1:
                   lista_dados[cont_data_list]['ano'] =2022
             elif lista_urls.index(lista_urls[cont_url_list])==2:
@@ -61,5 +61,24 @@ def construir_dataframe(time:str):
     
     return time_dataframe
 
+
+def construir_grafico(metrica: str, time1:str,time2:str):
+    df_time1=construir_dataframe(time1)
+    df_time2=construir_dataframe(time2)
+    
+    anos=[2021,2022]
+    
+    fig= go.Figure(
+        data=[
+           go.Bar(name=time1, x=anos,y=[df_time1['2021'][metrica], df_time1['2022'][metrica]]), 
+           go.Bar(name=time2, x=anos,y=[df_time2['2021'][metrica], df_time1['2022'][metrica]]) 
+        ],
+        
+        layout_title_text=metrica
+    )
+    
+    return fig.show()
+    
+    
 
 construir_dataframe('al-hilal')
