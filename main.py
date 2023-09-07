e7c1222f1db91e8727f9afcba4e33141e6364afb
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-09-07 16:37:42 -0300

    gerando dataframe para o ano de 2023

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import requests
 from bs4 import BeautifulSoup
 from lxml import html
+import pandas as pd
 
 
 endereco_time= {'al-hilal':'al-hilal/21895','al-taawoun':'al-taawoun/56021','al-ittihad':'al-ittihad/34315','al-ahli':'/al-ahli/34469','al-ettifaq':'al-ettifaq/34318','al-nassr':'al-nassr/23400','al-fateh':'al-fateh/56023','al-fayha':'al-fayha/168094','al-wehda':'al-wehda/32994','abha':'abha/168090','al-tai':'al-tai/168072','al-khaleej':'al-khaleej/167228','al-akhdood':'al-akhdood/336456','al-raed':'al-raed/56031','al-riyadh':'al-riyadh/168088','damac-fc':'damac-fc/204126','al-shabab':'al-shabab/34313','al-hazem':'al-hazem/168086'}
@@ -30,22 +31,35 @@ def escolhe_time(time:str):
     url_23=base_api + id_time + middle_api + end_point_23 + end_api
     url_21=base_api + id_time + middle_api + end_point_21 + end_api
     
-    lista_urls=[url_22 ,url_23 ,url_21]
+    lista_urls=[url_23]
     
     for url in lista_urls:
         api_link= requests.get(url,headers=browser).json()
         if not 'error' in api_link:
             lista_dados.append(api_link['statistics'])
             if lista_urls.index(lista_urls[cont_url_list])==0:
-                lista_dados[cont_data_list]['ano'] =2022
+                lista_dados[cont_data_list]['ano'] =2023
             elif lista_urls.index(lista_urls[cont_url_list])==1:
-                  lista_dados[cont_data_list]['ano'] =2023
+                  lista_dados[cont_data_list]['ano'] =2022
             elif lista_urls.index(lista_urls[cont_url_list])==2:
-                  lista_dados[cont_data_list]['ano'] =2021
+                  lista_dados[cont_data_list]['ano'] =2023
             cont_data_list +=1
         cont_url_list +=1
             
-        return lista_dados
+    return lista_dados
 
 
+def construir_dataframe(time:str):
+    team = escolhe_time(time)
+    
+    time_dataframe=pd.DataFrame(index=team[0].keys())
+    
+    for i in range(len(team)):
+        time_dataframe[str(team[i]['ano'])] = team[i].values()
+        
+    time_dataframe['Media']=time_dataframe.mean(axis=1).apply(lambda x: float('{:.1f}'.format(x)))
+    
+    return time_dataframe
 
+
+construir_dataframe('al-hilal')
